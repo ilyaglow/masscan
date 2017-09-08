884f14b1f823d756ed3a583d46a33070f5bf3556
Ilya Glotov <ilya.glotov@gmail.com>
2017-09-08 20:56:40 +0300

    Fix formatting

--- src/main.c
+++ src/main.c
@@ -955,14 +955,13 @@ receive_thread(void *v)
              * This is where we do the output
              */
 
-            redisCommand(parms->masscan->red,"LPUSH hosts %u.%u.%u.%u:%u",
-              (ip_them>>24)&0xFF,
-              (ip_them>>16)&0xFF,
-              (ip_them>> 8)&0xFF,
-              (ip_them>> 0)&0xFF,
-              port_them
+            redisCommand(parms->masscan->red, "LPUSH hosts %u.%u.%u.%u:%u",
+                (ip_them>>24)&0xFF,
+                (ip_them>>16)&0xFF,
+                (ip_them>> 8)&0xFF,
+                (ip_them>> 0)&0xFF,
+                port_them
             );
-
 
             output_report_status(
                         out,
